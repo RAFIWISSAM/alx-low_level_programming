239f07912996243206f2bde96d93c69eec60b256
WISSAM RAFI <wissamrafi00@gmail.com>
2023-04-20 09:13:34 -0700

    hello c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (succeess)
+ * Description - printing lines
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
