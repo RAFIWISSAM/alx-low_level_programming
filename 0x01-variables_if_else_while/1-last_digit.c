e3e09d967c387bcbb028ab3c593260685e0d61e4
WISSAM RAFI <wissamrafi00@gmail.com>
2023-04-21 07:43:23 -0700

    conditions

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -3,10 +3,10 @@
 #include <stdio.h>
 
 /**
- * main: Entry point.
+ * main - Entry point
  *
- * Return: Always 0.
- * Description - Printing lines
+ * Return: Always 0 (success)
+ * Description - Printing lines.
  */
 int main(void)
 {
