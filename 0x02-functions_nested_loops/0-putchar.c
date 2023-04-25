c0fed4cf03f66341f017e975ca247a196b4276c6
WISSAM RAFI <wissamrafi00@gmail.com>
2023-04-25 02:15:16 -0700

    functions

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point.
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	putchar ('_');
+	putchar ('p');
+	putchar ('u');
+	putchar ('t');
+	putchar ('c');
+	putchar ('h');
+	putchar ('a');
+	putchar ('r');
+	putchar ('\n');
+
+	return (0);
+}
