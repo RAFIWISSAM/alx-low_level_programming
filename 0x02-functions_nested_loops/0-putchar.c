1c77d0c9319e003de29ebce604c0055379985cea
WISSAM RAFI <wissamrafi00@gmail.com>
2023-04-25 04:10:04 -0700

    main.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,21 +1,20 @@
-#include <stdio.h>
+#include <main.h>
 /**
- * main - Entry point.
+ * main - Entry point
  *
  * Return: Always 0 (success)
  */
 
 int main(void)
 {
-	putchar ('_');
-	putchar ('p');
-	putchar ('u');
-	putchar ('t');
-	putchar ('c');
-	putchar ('h');
-	putchar ('a');
-	putchar ('r');
-	putchar ('\n');
-
-	return (0);
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+return (0);
 }
