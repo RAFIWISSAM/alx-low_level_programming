1dbae39e3b4ddec263d44a78aacbd6a660a5986c
WISSAM RAFI <wissamrafi00@gmail.com>
2023-04-25 05:23:41 -0700

    functions

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (sucess)
+ */
+void print_alphabet(void)
+{
+	char ch;
+	for ( ch = 'a' ; ch <= 'z' ; ch++ )
+{
+	_putchar(ch);
+	_putchar('\n');
+}
+}
