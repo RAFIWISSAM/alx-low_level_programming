b9d69f681d440c0cea3b18259937833d877399b4
WISSAM RAFI <wissamrafi00@gmail.com>
2023-04-26 08:18:16 -0700

    debugging

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,12 +1,16 @@
 #include "main.h"
+
 /**
 * main - Entry point
 * Return: 0
 */
+
 int main(void)
 {
-int i;
-i = 0;
-positive_or_negative(i);
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
 return (0);
 }
